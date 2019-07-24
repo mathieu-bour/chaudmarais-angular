f8845583b9f41a8e2ccb55dec138ab05ab03e940
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-07-24 17:13:56 +0200

    feat(express): add www => non-www and enforce https

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
 import * as express from 'express';
+import {redirectToHTTPS} from 'express-http-to-https';
 
 const port = process.env.PORT || 4000;
-const app = express();
 
+const app = express();
+app.enable('trust proxy');
+
+// Enforce HTTPS, if not running on localhost:PORT
+app.get('*', redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+// Enforce www to non-www redirection (source: https://stackoverflow.com/a/23816083)
+app.get('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    const newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  }
+
+  next();
+});
 
 app.get('*.*', express.static(__dirname, {maxAge: '1y'}));
 
