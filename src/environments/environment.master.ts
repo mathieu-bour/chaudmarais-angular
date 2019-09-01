0d14f52ccaff68d169bc4954855ee7f9c349069d
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-09-01 23:57:24 +0200

    chore(stripe): add live key to master env

--- src/environments/environment.master.ts
+++ src/environments/environment.master.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'https://chaudmarais.fr',
   api: 'https://api.chaudmarais.fr',
-  stripe_pk: 'pk_test_L2oDQpNIERMpMwLAOIvNaKcI00gvHSuzXZ'
+  stripe_pk: 'pk_live_P7wsFXpxWW49caZ1HxcwtLGf00GRDoW4Po'
 };
