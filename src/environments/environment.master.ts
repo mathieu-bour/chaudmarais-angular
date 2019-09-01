b2eeb11a78ce230462fb67f826da30b28fa73d0d
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-09-02 00:40:42 +0200

    fix(api): change dev api url

--- src/environments/environment.master.ts
+++ src/environments/environment.master.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://chaudmarais.fr',
-  api: 'https://api.chaudmarais.fr',
+  api: 'https://dev.api.chaudmarais.fr',
   stripe_pk: 'pk_live_P7wsFXpxWW49caZ1HxcwtLGf00GRDoW4Po'
 };
