256cb633a31b3b51734bd6ebf795a268ada44bb8
Mathieu Bour <mathieu.tin.bour@gmail.com>
2019-11-23 20:01:31 +0100

    fix(products): change min height

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   url: 'https://dev.chaudmarais.fr',
-  api: 'https://api.chaudmarais.fr',
+  api: 'https://dev.api.chaudmarais.fr',
   stripe_pk: 'pk_test_TyUOW9CGIqZv9xne8Pzhd1PE00EyQAIKie'
 };
