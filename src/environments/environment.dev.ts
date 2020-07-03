1cce862bbeeca3b9424d76f181e6f51145ad9864
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-07-04 00:10:09 +0200

    fix: change dev stripe environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   url: 'https://dev.chaudmarais.fr',
   api: 'https://dev.api.chaudmarais.fr',
-  stripe_pk: 'pk_test_L2oDQpNIERMpMwLAOIvNaKcI00gvHSuzXZ'
+  stripe_pk: 'pk_test_TyUOW9CGIqZv9xne8Pzhd1PE00EyQAIKie'
 };
