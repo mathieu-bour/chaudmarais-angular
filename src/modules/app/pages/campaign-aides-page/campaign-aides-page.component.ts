b46d051d26fa1d0cb6cf55fdc1e7bc7ae0dd7592
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-07-04 00:49:22 +0200

    feat: split aides and default products

---
+++ src/modules/app/pages/campaign-aides-page/campaign-aides-page.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-aides-page',
+  templateUrl: './campaign-aides-page.component.html',
+  styleUrls: ['./campaign-aides-page.component.scss'],
+})
+export class CampaignAidesPageComponent {
+}
--- src/modules/app/pages/campaign-page/campaign-page.component.ts
+++ src/modules/app/pages/campaign-page/campaign-page.component.ts
@@ -1,15 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-campaign-page',
   templateUrl: './campaign-page.component.html',
   styleUrls: ['./campaign-page.component.scss']
 })
-export class CampaignPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CampaignPageComponent {
 }
