9bdfe7d5beb191e646406f9ac70a21b5a64386ea
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-07-04 03:21:41 +0200

    feat: add aides campaign page

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,30 +1,34 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
+import {
+  ExtendedModule,
+  FlexModule,
+} from '@angular/flex-layout';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
-import {routes} from './app.routes';
-import {NgxImageZoomModule} from 'ngx-image-zoom';
-import {NgxsModule} from '@ngxs/store';
-import {environment} from '../../environments/environment';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {NgxsStoragePluginModule} from '@ngxs/storage-plugin';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GrpdDialogComponent} from './components/grpd-dialog/grpd-dialog.component';
-import {AppState} from './state/app.state';
+import {NgxsModule} from '@ngxs/store';
+import {NgxImageZoomModule} from 'ngx-image-zoom';
+import {environment} from '../../environments/environment';
 import {APIModule} from '../api/api.module';
+import {UIModule} from '../ui/ui.module';
+import {AppComponent} from './app.component';
+import {routes} from './app.routes';
+import {GrpdDialogComponent} from './components/grpd-dialog/grpd-dialog.component';
+import {CampaignAidesPageComponent} from './pages/campaign-aides-page/campaign-aides-page.component';
 import {CampaignPageComponent} from './pages/campaign-page/campaign-page.component';
+import {CommitmentsPageComponent} from './pages/commitments-page/commitments-page.component';
+import {ContactPageComponent} from './pages/contact-page/contact-page.component';
+import {FirstPageComponent} from './pages/first-page/first-page.component';
 import {HistoryPageComponent} from './pages/history-page/history-page.component';
 import {LegalPageComponent} from './pages/legal-page/legal-page.component';
 import {ThanksPageComponent} from './pages/thanks-page/thanks-page.component';
-import {BannerComponent} from '../ui/components/banner/banner.component';
-import {UIModule} from '../ui/ui.module';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ExtendedModule, FlexModule} from '@angular/flex-layout';
-import { FirstPageComponent } from './pages/first-page/first-page.component';
-import { CommitmentsPageComponent } from './pages/commitments-page/commitments-page.component';
-import { CampaignAidesPageComponent } from './pages/campaign-aides-page/campaign-aides-page.component';
+import {AppState} from './state/app.state';
 
 @NgModule({
   declarations: [
@@ -39,7 +43,7 @@ import { CampaignAidesPageComponent } from './pages/campaign-aides-page/campaign
     ContactPageComponent,
     FirstPageComponent,
     CommitmentsPageComponent,
-    CampaignAidesPageComponent
+    CampaignAidesPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,16 +55,16 @@ import { CampaignAidesPageComponent } from './pages/campaign-aides-page/campaign
     NgxsModule.forRoot([AppState], {developmentMode: !environment.production}),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsStoragePluginModule.forRoot({
-      key: ['app', 'auth', 'cart']
+      key: ['app', 'auth', 'cart'],
     }),
     APIModule,
     UIModule,
     FlexModule,
-    ExtendedModule
+    ExtendedModule,
   ],
   entryComponents: [GrpdDialogComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
--- src/modules/app/app.routes.ts
+++ src/modules/app/app.routes.ts
@@ -1,62 +1,67 @@
 import {Routes} from '@angular/router';
 import {LoggedGuard} from '../account/guards/logged.guard';
-import { CommitmentsPageComponent } from './pages/commitments-page/commitments-page.component';
-import {LegalPageComponent} from './pages/legal-page/legal-page.component';
-import {ThanksPageComponent} from './pages/thanks-page/thanks-page.component';
-import {HistoryPageComponent} from './pages/history-page/history-page.component';
+import {CampaignAidesPageComponent} from './pages/campaign-aides-page/campaign-aides-page.component';
 import {CampaignPageComponent} from './pages/campaign-page/campaign-page.component';
+import {CommitmentsPageComponent} from './pages/commitments-page/commitments-page.component';
 import {ContactPageComponent} from './pages/contact-page/contact-page.component';
 import {FirstPageComponent} from './pages/first-page/first-page.component';
+import {HistoryPageComponent} from './pages/history-page/history-page.component';
+import {LegalPageComponent} from './pages/legal-page/legal-page.component';
+import {ThanksPageComponent} from './pages/thanks-page/thanks-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: FirstPageComponent
+    component: FirstPageComponent,
   },
   {
     path: 'campagne',
-    component: CampaignPageComponent
+    component: CampaignPageComponent,
+  },
+  {
+    path: 'aides',
+    component: CampaignAidesPageComponent,
   },
   {
     path: 'engagements',
-    component: CommitmentsPageComponent
+    component: CommitmentsPageComponent,
   },
   {
     path: 'histoire',
-    component: HistoryPageComponent
+    component: HistoryPageComponent,
   },
   {
     path: 'mentions-legales',
-    component: LegalPageComponent
+    component: LegalPageComponent,
   },
   {
     path: 'remerciements',
-    component: ThanksPageComponent
+    component: ThanksPageComponent,
   },
   {
     path: 'contact',
-    component: ContactPageComponent
+    component: ContactPageComponent,
   },
   {
     path: 'eshop',
-    loadChildren: () => import('../shop/shop.module').then(mod => mod.ShopModule)
+    loadChildren: () => import('../shop/shop.module').then(mod => mod.ShopModule),
   },
   {
     path: 'paiement',
     loadChildren: () => import('../checkout/checkout.module').then(mod => mod.CheckoutModule),
-    canActivate: [LoggedGuard]
+    canActivate: [LoggedGuard],
   },
   {
     path: 'compte',
     loadChildren: () => import('../account/account.module').then(mod => mod.AccountModule),
-    canActivate: [LoggedGuard]
+    canActivate: [LoggedGuard],
   },
   {
     path: 'admin',
-    loadChildren: () => import('../admin/admin.module').then(mod => mod.AdminModule)
+    loadChildren: () => import('../admin/admin.module').then(mod => mod.AdminModule),
   },
   {
     path: '**',
-    redirectTo: 'eshop'
-  }
+    redirectTo: 'eshop',
+  },
 ];
