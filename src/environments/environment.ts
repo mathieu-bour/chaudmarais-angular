6b0a52c6371fbbcdd4f4e9aa0b9cf3f9ddf99c87
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-09-14 12:00:04 +0200

    feat(cdn): use google cloud storage instead

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   url: 'https://dev.chaudmarais.fr',
-  api: 'http://localhost:8080',
+  api: 'https://dev.api.chaudmarais.fr',
+  cdn: 'https://storage.googleapis.com/cdn.chaudmarais.fr',
   stripe_pk: 'pk_test_TyUOW9CGIqZv9xne8Pzhd1PE00EyQAIKie'
 };
